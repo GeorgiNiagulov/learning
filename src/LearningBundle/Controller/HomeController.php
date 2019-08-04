020c72b03fa9fbffa02a1ed86da6ff7754302d95
Georgi <gnqgulov@abv.bg>
2019-08-04 18:33:49 +0300

    add migrations, entity, register, login

---
+++ src/LearningBundle/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LearningBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="blog_index")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html.twig');
+    }
+}
--- web/js/main.js
+++ web/js/main.js
@@ -556,7 +556,7 @@
 				enabled:true
 			},
 			zoom: {
-				enabled: true, // By default it's false, so don't forget to enable it
+				enabled: true, // By home it's false, so don't forget to enable it
 
 				duration: 300, // duration of the effect, in milliseconds
 				easing: 'ease-in-out', // CSS transition easing function
